a28c904528bcfc1bef3c3fc156b21283afe33e02
tanakorn0314 <tanakorn0412@gmail.com>
2018-12-25 11:28:53 +0700

    update webhook

--- server.js
+++ server.js
@@ -8,8 +8,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello Server');
+});
+
+app.post('/webhook', (req, res) => {
+    let replyToken = req.body.events[0].replyToken;
+    let msg = req.body.events[0].message.text;
+
+    console.log(`Message token : ${replyToken}`);
+    console.log(`Message : ${msg}`);
+
+    res.json({
+        status: '200',
+        message: 'Webhook is working!!'
+    });
 });
 
 app.listen(PORT, () => {
